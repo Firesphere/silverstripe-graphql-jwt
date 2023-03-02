791ae1864b401633fad8ade7214acb3f6c6f6b80
Viktor Åsbrink <viktor@kreationsbyran.se>
2023-03-02 16:23:33 +0100

    Fix MutationResultGenerator not handling SS errors

--- src/Helpers/MutationResultGenerator.php
+++ src/Helpers/MutationResultGenerator.php
@@ -2,13 +2,23 @@
 
 namespace Firesphere\GraphQLJWT\Helpers;
 
-use Firesphere\GraphQLJWT\Resolvers\Resolver;
 
 trait MutationResultGenerator
 {
+
+  private static function getFirstMessage(array $messages)
+  {
+    return array_map(function ($message) {
+      if (gettype($message) === "string") return $message;
+      if (gettype($message) === "array" && isset($message['message'])) return $message['message'];
+      return 'Something unexpected went wrong with your request, contact support to recieve help';
+    }, $messages);
+  }
+
+
   public static function generateResultResponse(string $result, array $messages = []): array
   {
-    $messages = count($messages) ? $messages : [ErrorMessageGenerator::getResultMessage($result)];
+    $messages = count($messages) ? self::getFirstMessage($messages) : [ErrorMessageGenerator::getResultMessage($result)];
     return [
       'result' => $result,
       'message' => $messages,
